cd52ced57c4e378c4ec4772866006d8dcb5c4c4f
84k491 <you@example.com>
2019-01-20 18:51:15 +0300

    Animated menu

--- app/src/main/java/bakar/labirynth/MenuActivity.java
+++ app/src/main/java/bakar/labirynth/MenuActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -17,46 +21,35 @@ import java.util.Random;
 
 public class MenuActivity extends Activity implements OnClickListener {
 
+    boolean justLoadedState;
+
+    SharedPreferences sPref;
+    ConstraintLayout layout;
     Button start;
     Switch joystick;
     Switch debug;
     Switch fog;
     EditText xsize;
     EditText ysize;
-    SharedPreferences sPref;
     TextView gold;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_menu);
-
-        TextView tx = findViewById(R.id.main_title);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/CLiCHE 21.ttf");
-        tx.setTypeface(custom_font);
-
-        ysize = findViewById(R.id.ysize);
-        xsize = findViewById(R.id.xsize);
-        start = findViewById(R.id.start);
-        start.setOnClickListener(this);
-        joystick = findViewById(R.id.joystick);
-        joystick.setOnClickListener(this);
-        debug = findViewById(R.id.debug);
-        debug.setOnClickListener(this);
-        fog = findViewById(R.id.fog);
-        fog.setOnClickListener(this);
-        gold = findViewById(R.id.gold);
-    }
+    TextView title;
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.start:
-                startGameActivity();
-                break;
-            default: break;
+        if (justLoadedState){
+            justLoadedState = false;
+            startOnTouchAnimations();
         }
+        else
+        {
+            switch (view.getId()){
+                case R.id.start:
+                    startGameActivity();
+                    break;
+                default: break;
+            }
+        }
+
     }
 
     void startGameActivity(){
@@ -71,6 +64,30 @@ public class MenuActivity extends Activity implements OnClickListener {
         startActivity(intent);
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_menu);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/CLiCHE 21.ttf");
+        title = findViewById(R.id.main_title);
+        ysize = findViewById(R.id.ysize);
+        xsize = findViewById(R.id.xsize);
+        start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+        joystick = findViewById(R.id.joystick);
+        joystick.setOnClickListener(this);
+        debug = findViewById(R.id.debug);
+        debug.setOnClickListener(this);
+        fog = findViewById(R.id.fog);
+        fog.setOnClickListener(this);
+        gold = findViewById(R.id.gold);
+        layout = findViewById(R.id.menu_layout);
+        layout.setOnClickListener(this);
+
+        title.setTypeface(custom_font);
+    }
     protected void onRestart(){
         super.onRestart();
     }
@@ -82,6 +99,8 @@ public class MenuActivity extends Activity implements OnClickListener {
     protected void onResume(){
         super.onResume();
         loadData();
+        justLoadedState = true;
+        startWelcomingAnimations();
     }
     @Override
     protected void onPause(){
@@ -107,7 +126,6 @@ public class MenuActivity extends Activity implements OnClickListener {
         ed.putBoolean("fog_enabled", fog.isChecked());
         ed.commit(); //ed.apply();
     }
-
     void loadData() {
         sPref = getSharedPreferences("global", MODE_PRIVATE);
         joystick.setChecked(sPref.getBoolean("uses_joystick", true));
@@ -117,5 +135,34 @@ public class MenuActivity extends Activity implements OnClickListener {
         xsize.setText(String.valueOf(sPref.getInt("xsize", 42)));
         ysize.setText(String.valueOf(sPref.getInt("ysize", 42)));
         gold.setText(String.valueOf(sPref.getInt("gold", 0)));
+    }
+    void startWelcomingAnimations(){
+        Animation move_anim = AnimationUtils.loadAnimation(this, R.anim.keep_title_offset);
+        Animation inv_anim = AnimationUtils.loadAnimation(this, R.anim.keep_invisible);
+        title.startAnimation(move_anim);
+
+        start.startAnimation(inv_anim);
+        joystick.startAnimation(inv_anim);
+        debug.startAnimation(inv_anim);
+        fog.startAnimation(inv_anim);
+        xsize.startAnimation(inv_anim);
+        ysize.startAnimation(inv_anim);
+        gold.startAnimation(inv_anim);
+    }
+    void startOnTouchAnimations(){
+        Animation move_anim = AnimationUtils.loadAnimation(this, R.anim.title_move);
+        //Animation inv_anim = AnimationUtils.loadAnimation(this, R.anim.unfade);
+        Animation inv_anim = new AlphaAnimation(0.0f, 1.0f);
+        inv_anim.setDuration(3000);
+        inv_anim.setStartOffset(700);
+        title.startAnimation(move_anim);
+
+        start.startAnimation(inv_anim);
+        joystick.startAnimation(inv_anim);
+        debug.startAnimation(inv_anim);
+        fog.startAnimation(inv_anim);
+        xsize.startAnimation(inv_anim);
+        ysize.startAnimation(inv_anim);
+        gold.startAnimation(inv_anim);
     }
 }
